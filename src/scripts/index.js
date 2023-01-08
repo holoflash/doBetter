ff90ed256dd6d82380a56b16a09f59d1bdfc0abd
holoflash <mikepalacemusic@gmail.com>
2023-01-08 12:57:20 +0100

    Test local storage

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,9 +5,9 @@ import { initializer } from "./initializer";
 import { variableContentCreator } from "./variableContentCreator";
 
 staticContentCreator();
+initializer();
 
 const storedObject = JSON.parse(localStorage.getItem('Readme'));
 
 variableContentCreator(storedObject.name, storedObject.color);
-initializer();
 
